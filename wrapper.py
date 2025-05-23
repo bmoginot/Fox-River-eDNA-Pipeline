e5598dea6d228e278f1ae074d0e69a3cf4512635
Ben Moginot <bmoginot5@gmail.com>
2025-05-23 12:12:18 -0500

    subprocess run for cutadapt + absolute path grabbing

--- wrapper.py
+++ wrapper.py
@@ -9,15 +9,16 @@ def get_args(args=None):
     parser.add_argument("-i", "--input", help="directory containing reads", required=True)
     return parser.parse_args(args)
 
-def trim(reads=None, primers=None, log=None):
+def trim(reads=None, primers=None, outdir=None, log=None):
     """
     trim reads
     first, creates directory to store trimmed reads. then, iterates through input directory and pulls out each read pair.
     then, puts each read pair through cutadapt with input primers and error rate. cut adapt trims the primers from the reads
     and writes the new fq files to the newly-created directory.
     """
-    os.mkdir("trimmed_reads")
-    os.chdir("trimmed_reads")
+    trimmed_reads_dir = outdir + "trimmed_reads"
+    os.mkdir(trimmed_reads_dir)
+    os.chdir(trimmed_reads_dir)
     path = reads + "*"
     files = sorted(glob.glob(path))
 
@@ -32,27 +33,34 @@ def trim(reads=None, primers=None, log=None):
             ["cutadapt", "-g", primers[0], "-G", primers[1], "-e", erate, "-o", fout, "-p", rout, fread, rread],
             stdout=log # write output to log
         )
+    
+    trimmed_reads_dir = os.getcwd()
+    return trimmed_reads_dir
 
-def run_dada2(reads=None):
-    pass
+def run_dada2(reads=None, outdir=None, log=None):
+    print(reads)
+    print(outdir)
+    print(log)
 
 def main():
     args = get_args(sys.argv[1:])
-    cwd = os.getcwd()
-    reads_dir = os.path.join(cwd, args.input) # store path to input data directory
+    project_dir = os.getcwd()
+    reads_dir = os.path.join(project_dir, args.input) # store path to input data directory
 
-    outdir = "eDNA_pipeline_output"
+    outdir = os.path.join(project_dir, "eDNA_pipeline_output/")
     if os.path.isdir(outdir):
         os.system(f"rm -r {outdir}")
     os.mkdir(outdir) # create output directory and move into it
-    os.chdir(outdir)
+    print(project_dir)
+    print(outdir)
 
-    log = open("wrapper.log", "w") # open log
+    log = open(outdir + "wrapper.log", "w") # open log
 
     primers = ("ACTGGGATTAGATACCCC", "TAGAACAGGCTCCTCTAG")
-    trim(reads_dir, primers, log)
+    trimmed_reads_dir = trim(reads_dir, primers, outdir, log)
+    os.chdir(project_dir) # return to project dir after running scripts and creating output
 
-    # run_dada2(reads_dir)
+    run_dada2(trimmed_reads_dir, outdir, log)
 
     log.close()
 
