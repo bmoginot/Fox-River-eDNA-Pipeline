b571be79234704a84849498575cf1d7314b69945
Ben Moginot <bmoginot5@gmail.com>
2025-05-23 14:57:54 -0500

    function call for dada2 script works + removed chdir calls for cutadapt

--- wrapper.py
+++ wrapper.py
@@ -16,9 +16,8 @@ def trim(reads=None, primers=None, outdir=None, log=None):
     then, puts each read pair through cutadapt with input primers and error rate. cut adapt trims the primers from the reads
     and writes the new fq files to the newly-created directory.
     """
-    trimmed_reads_dir = outdir + "trimmed_reads"
+    trimmed_reads_dir = os.path.join(outdir, "trimmed_reads")
     os.mkdir(trimmed_reads_dir)
-    os.chdir(trimmed_reads_dir)
     path = reads + "*"
     files = sorted(glob.glob(path))
 
@@ -27,40 +26,42 @@ def trim(reads=None, primers=None, outdir=None, log=None):
     for i in range(0, len(files), 2): # REMOVE HARCODING LATER; LET THEM PICK PRIMERS AND ERROR RATE
         fread = files[i]
         rread = files[i+1]
-        fout = fread.split("/")[-1].split(".")[0] + "-trimmed.fastq"
-        rout = rread.split("/")[-1].split(".")[0] + "-trimmed.fastq"
+        fout = os.path.join(trimmed_reads_dir, fread.split("/")[-1].split(".")[0] + "-trimmed.fastq")
+        rout = os.path.join(trimmed_reads_dir, rread.split("/")[-1].split(".")[0] + "-trimmed.fastq")
         subprocess.run(
             ["cutadapt", "-g", primers[0], "-G", primers[1], "-e", erate, "-o", fout, "-p", rout, fread, rread],
             stdout=log # write output to log
         )
-    
-    trimmed_reads_dir = os.getcwd()
+
     return trimmed_reads_dir
 
-def run_dada2(reads=None, outdir=None, log=None):
-    print(reads)
-    print(outdir)
-    print(log)
+def run_dada2(reads=None, outdir=None, log=None, table=None):
+    subprocess.run(
+            ["Rscript", "src/denoise_reads.R", "-i", reads, "-o", outdir, "-t", table],
+            stdout=log
+        )
+    
+    return os.path.join(outdir, table)
 
 def main():
     args = get_args(sys.argv[1:])
     project_dir = os.getcwd()
     reads_dir = os.path.join(project_dir, args.input) # store path to input data directory
 
-    outdir = os.path.join(project_dir, "eDNA_pipeline_output/")
+    outdir = os.path.join(project_dir, "eDNA_pipeline_output")
     if os.path.isdir(outdir):
         os.system(f"rm -r {outdir}")
     os.mkdir(outdir) # create output directory and move into it
-    print(project_dir)
-    print(outdir)
 
-    log = open(outdir + "wrapper.log", "w") # open log
+    log = open(os.path.join(outdir, "wrapper.log"), "w") # open log
 
     primers = ("ACTGGGATTAGATACCCC", "TAGAACAGGCTCCTCTAG")
     trimmed_reads_dir = trim(reads_dir, primers, outdir, log)
-    os.chdir(project_dir) # return to project dir after running scripts and creating output
 
-    run_dada2(trimmed_reads_dir, outdir, log)
+    table = "asv_table.tsv" # name of asv table to store for later
+
+    asv_table = run_dada2(trimmed_reads_dir, outdir, log, table)
+    print(asv_table)
 
     log.close()
 
