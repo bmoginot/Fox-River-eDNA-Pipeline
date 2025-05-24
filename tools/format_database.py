5dea32c385b9eede3d01a2b7a9b27e3eb5e86519
Ben Moginot <bmoginot5@gmail.com>
2025-05-24 12:53:55 -0500

    test --> data

--- tools/format_database.py
+++ tools/format_database.py
@@ -1,9 +1,9 @@
 # change database file from tsv to fasta for use in vsearch
 import pandas as pd
 
-tax_db = pd.read_csv("test/database/12S_Kankakee_db_1117.tsv", sep="\t")
+tax_db = pd.read_csv("data/database/12S_Kankakee_db_1117.tsv", sep="\t")
 
-with open("test/database/vsearch_ref_seqs.fasta", "w") as f:
+with open("data/database/vsearch_ref_seqs.fasta", "w") as f:
     for row in tax_db.itertuples(): # iterate through df, extract ids and sequences and write in fasta format
         id = ">" + row.id
         seq = row.Sequence
@@ -12,4 +12,4 @@ with open("test/database/vsearch_ref_seqs.fasta", "w") as f:
 
 # drop sequence, write taxa map to tsv
 tax_db_noseq = tax_db.drop(labels="Sequence", axis=1)
-tax_db_noseq.to_csv("test/database/vsearch_ref_taxa.tsv", sep="\t", index=False)
+tax_db_noseq.to_csv("data/database/vsearch_ref_taxa.tsv", sep="\t", index=False)
