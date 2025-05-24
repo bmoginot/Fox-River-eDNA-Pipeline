acdfa275300fbab4e359f6394e8d8fdb9589ed6e
Ben Moginot <bmoginot5@gmail.com>
2025-05-24 12:13:13 -0500

    writes out taxa map tsv as well

--- tools/format_database.py
+++ tools/format_database.py
@@ -3,9 +3,13 @@ import pandas as pd
 
 tax_db = pd.read_csv("test/database/12S_Kankakee_db_1117.tsv", sep="\t")
 
-with open("test/database/db_formatted_for_vsearch.fasta", "w") as f:
+with open("test/database/vsearch_ref_seqs.fasta", "w") as f:
     for row in tax_db.itertuples(): # iterate through df, extract ids and sequences and write in fasta format
         id = ">" + row.id
         seq = row.Sequence
         f.write(f"{id}\n")
         f.write(f"{seq}\n")
+
+# drop sequence, write taxa map to tsv
+tax_db_noseq = tax_db.drop(labels="Sequence", axis=1)
+tax_db_noseq.to_csv("test/database/vsearch_ref_taxa.tsv", sep="\t", index=False)
