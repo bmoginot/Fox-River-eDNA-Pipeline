a42e2492e206f51e4134c44006f109b55597d7c8
Ben Moginot <bmoginot5@gmail.com>
2025-05-26 09:25:34 -0500

    imports tsv into qiime

--- tools/qiime_format_database.py
+++ tools/qiime_format_database.py
@@ -1,11 +1,22 @@
 # format database for use with vsearch in qiime2
 
 import pandas as pd
+import subprocess
+import os
 
 tax_db = pd.read_csv("data/database/12S_Kankakee_db_1117.tsv", sep="\t") # read in database
 
+taxa_tsv = os.path.join(os.getcwd(), "data", "database", "kankakee_ref.tsv")
+
 # drop sequence, write taxa map to tsv
 tax_db_noseq = tax_db.drop(labels="id", axis=1)
-tax_db_noseq.to_csv("data/database/qiime_ref_taxa_for_vsearch.tsv", sep="\t",
-                    index=False, header=["Feature ID", "Taxon"])
+tax_db_noseq.to_csv(taxa_tsv, sep="\t", index=False, header=["Feature ID", "Taxon"])
+                    
+ref_qza = os.path.join(os.getcwd(), "data", "database", "ref_for_qiime_vsearch.qza")
 
+subprocess.run([
+    "qiime", "tools", "import",
+    "--type", "FeatureData[Taxonomy]",
+    "--input-format", "TSVTaxonomyFormat",
+    "--input-path", taxa_tsv,
+    "--output-path", ref_qza])
