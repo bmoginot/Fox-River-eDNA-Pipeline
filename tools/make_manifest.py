462692fdfce592bba1638aac6416ee377933a9bd
Ben Moginot <bmoginot5@gmail.com>
2025-05-26 08:04:23 -0500

    make maifest file for qiime2 import

---
+++ tools/make_manifest.py
@@ -0,0 +1,20 @@
+import os
+import glob
+import pandas as pd
+
+subset_reads = os.path.join(os.getcwd(), "data", "subset_reads", "*")
+manifest_file = os.path.join(os.getcwd(), "output", "kankakee_manifest.tsv")
+
+paths = sorted(glob.glob(subset_reads))
+
+data = {"sample-id": [], "forward_absolute_path": [], "reverse_absolute_path": []}
+
+sample_num = 1
+for i in range(0, len(paths), 2):
+    data["sample-id"].append("sample-" + str(sample_num))
+    data["forward_absolute_path"].append(paths[i])
+    data["reverse_absolute_path"].append(paths[i+1])
+    sample_num += 1
+
+man_df = pd.DataFrame(data)
+man_df.to_csv(manifest_file, sep="\t")
