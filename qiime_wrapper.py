2757be7aba1a6bc1b8625d720eea4fc7cd23c6c4
Ben Moginot <bmoginot5@gmail.com>
2025-05-26 20:22:40 -0500

    vsearch works, just need to test with the rest of the pipeline

--- qiime_wrapper.py
+++ qiime_wrapper.py
@@ -58,16 +58,38 @@ def trim_reads(reads=None, outdir=None, primers=None, log=None):
 
 def denoise_reads(trimmed_reads=None, outdir=None, log=None):
     """denoise reads using dada2"""
+    asv_seqs = os.path.join(outdir, "asv-seqs.qza")
+
     subprocess.run([
         "qiime", "dada2", "denoise-paired",
         "--i-demultiplexed-seqs", trimmed_reads,
         "--p-trunc-len-f", "95",
         "--p-trunc-len-r", "95",
-        "--o-representative-sequences", os.path.join(outdir, "asv-sequences-0.qza"),
+        "--o-representative-sequences", asv_seqs,
         "--o-table", os.path.join(outdir, "feature-table-0.qza"),
         "--o-denoising-stats", os.path.join(outdir, "dada2-stats.qza")],
         stdout=log,
         stderr=log)
+    
+    return asv_seqs
+    
+def run_vsearch(asv_seqs=None, ref_seqs=None, ref_taxa=None, outdir=None, log=None):
+    out_taxa = os.path.join(outdir, "vsearch_taxa.qza")
+    top_hits = os.path.join(outdir, "vsearch_top_hits.qza")
+
+    subprocess.run([
+        "qiime", "feature-classifier", "classify-consensus-vsearch",
+        "--i-query", asv_seqs,
+        "--i-reference-reads", ref_seqs,
+        "--i-reference-taxonomy", ref_taxa,
+        "--p-perc-identity", "1.0",
+        "--p-min-consensus", "0.94",
+        "--o-classification", out_taxa,
+        "--o-search-results", top_hits],
+        stdout=log,
+        stderr=log)
+    
+    # return out_taxa
 
 def main():
     args = get_args(sys.argv[1:]) # get command line arguments
@@ -75,21 +97,27 @@ def main():
     project_dir = os.getcwd()
     outdir = os.path.join(project_dir, "output")
 
-    if os.path.isdir(outdir):
-        os.system(f"rm -r {outdir}")
-    os.mkdir(outdir) # make directory to store output
+    # if os.path.isdir(outdir):
+    #     os.system(f"rm -r {outdir}")
+    # os.mkdir(outdir) # make directory to store output
 
     log = open(os.path.join(outdir, "wrapper.log"), "w") # open log
 
-    reads = os.path.join(project_dir, args.input) # path to reads from arguments
+    # reads = os.path.join(project_dir, args.input) # path to reads from arguments
 
-    qiime_archive = make_manifest(reads, outdir, log)
+    # qiime_archive = make_manifest(reads, outdir, log)
 
-    primers = ("ACTGGGATTAGATACCCC", "TAGAACAGGCTCCTCTAG")
+    # primers = ("ACTGGGATTAGATACCCC", "TAGAACAGGCTCCTCTAG")
 
-    trimmed_reads = trim_reads(qiime_archive, outdir, primers, log)
+    # trimmed_reads = trim_reads(qiime_archive, outdir, primers, log)
 
-    denoise_reads(trimmed_reads, outdir, log)
+    # asv_seqs = denoise_reads(trimmed_reads, outdir, log)
+
+    asv_seqs = os.path.join(outdir, "asv-sequences-0.qza")
+    ref_seqs = os.path.join(project_dir, "data", "database", "seq_ref_for_qiime_vsearch.qza")
+    ref_taxa = os.path.join(project_dir, "data", "database", "taxa_ref_for_qiime_vsearch.qza")
+
+    run_vsearch(asv_seqs, ref_seqs, ref_taxa, outdir, log)
 
 if __name__ == "__main__":
     main()
