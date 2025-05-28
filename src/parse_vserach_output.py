62da8d6e7f80b0c12bb0e0d6a07bd88a0606d232
Ben Moginot <bmoginot5@gmail.com>
2025-05-28 11:49:06 -0500

    separates tsv into family-level and unassigned seqs

--- src/parse_vserach_output.py
+++ src/parse_vserach_output.py
@@ -19,7 +19,24 @@ def parse_vsearch_output():
 
     unzip_qza(vsearch_out, unzipped_vsearch) # extract taxonomy tsv from qiime archive
 
-    taxa_vsearch = pd.read_csv(unzipped_vsearch, sep="\t")
-    print(taxa_vsearch.head())
+    taxa_vsearch = pd.read_csv(unzipped_vsearch, sep="\t") # read in vsearch output taxonomy.tsv
+
+    unassigned = taxa_vsearch[
+        taxa_vsearch["Taxon"] # take the column with taxonomic classification
+        .str.split(";", expand=False) # split it into a list of taxonomic levels
+        .apply(lambda x: len(x) < 5) # save the rows where there are fewer than 5 levels (less than family-level)
+        ]
+    
+    unassigned.to_csv("unassigned_vsearch_taxa.tsv", sep="\t", index=False)
+    
+    retained = taxa_vsearch.drop( # drop all rows shared with unassigned (retain only family-level classifications)
+        taxa_vsearch[
+            taxa_vsearch["Feature ID"]
+            .isin(unassigned["Feature ID"])
+        ]
+        .index
+    )
+    
+    retained.to_csv("retained_vsearch_taxa.tsv", sep="\t", index=False)
 
 parse_vsearch_output()
