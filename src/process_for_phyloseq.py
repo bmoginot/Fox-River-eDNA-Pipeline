b98213ae31352a62613981870f61c0b154d82149
Ben Moginot <bmoginot5@gmail.com>
2025-06-12 10:04:31 -0500

    added mafft & fasttree qiime call + added comments & docstrings

--- src/process_for_phyloseq.py
+++ src/process_for_phyloseq.py
@@ -27,6 +27,7 @@ def stitch_taxa(dir):
     os.remove(final_taxa)
 
 def extract_qza(file, dir):
+    """unzip qiime archive for formatting"""
     os.mkdir("tmp")
     os.system(f"unzip -qd tmp {file}")
     unzipped = glob.glob("tmp/*/data/*")[0]
@@ -36,6 +37,7 @@ def extract_qza(file, dir):
     return os.path.join(dir, os.path.split(unzipped)[-1])
 
 def trim_fastas(dir):
+    """grab fasta output from dada2 and remove sequences that were left unclassified after taxonomy steps"""
     file = os.path.join("output", "dada2", "asv-seqs.qza")
     dada2_seqs = extract_qza(file, dir)
 
@@ -66,14 +68,30 @@ def trim_fastas(dir):
                 f.write(f">{feat}\n")
                 f.write(f"{asv_map[feat]}\n")
 
+    achive_out = os.path.join(dir, "final_seqs.qza")
+
     subprocess.run([
         "qiime", "tools", "import",
         "--type", "FeatureData[Sequence]",
         "--input-path", final_seqs,
-        "--output-path", os.path.join(dir, "final_seqs")
+        "--output-path", achive_out
     ])
 
     os.remove(final_seqs)
+
+    return achive_out
+
+def align_to_tree(file, dir):
+    """run mafft and fasttree in qiime to generate rooted tree for phyloseq"""
+    subprocess.run([
+          "qiime", "phylogeny", "align-to-tree-mafft-fasttree",
+          "--i-sequences", file,
+          "--p-n-threads", "12",
+          "--o-alignment", os.path.join(dir, "unmaksed_alignment"),
+          "--o-masked-alignment", os.path.join(dir, "masked_alignment"),
+          "--o-tree", os.path.join(dir, "unrooted_tree"),
+          "--o-rooted-tree", os.path.join(dir, "rooted_tree")
+    ])
 
 def main():
     dir = "phyloseq_input"
@@ -81,11 +99,11 @@ def main():
         os.system(f"rm -r {dir}")
     os.mkdir(dir)
 
+    os.system(f"cp output/dada2/feature-table.qza {dir}") # move feature table from dada2 output
     format_metadata(dir)
     stitch_taxa(dir)
-    trim_fastas(dir)
-    # extract_qza(os.path.join("output", "dada2", "feature-table.qza"), dir) # wait i need this as an archive
-    os.system(f"cp output/dada2/feature-table.qza {dir}")
+    fasta = trim_fastas(dir)
+    align_to_tree(fasta, dir)
 
 if __name__ == "__main__":
     main()
